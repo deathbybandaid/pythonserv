fcb50dc435c1a834f3abb6fbf4b1765f848e1cee
deathbybandaid <sam@deathbybandaid.net>
2019-10-10 13:17:59 -0400

    fix self.config

--- pythonserv/pythonserv.py
+++ pythonserv/pythonserv.py
@@ -7,28 +7,28 @@ import configparser
 
 class PythonSERVer():
     def __init__(self):
+        self.configfile = "/opt/pythonserv.cfg"
         print("Loading Config File")
-        self.config = config_file_to_dict("/opt/pythonserv.cfg")
+        self.config = self.config_file_to_dict(self.configfile)
         print(str(self.config))
+
+    def config_file_to_dict(filetoread):
+
+        newdict = dict()
+
+        # Read configuration
+        config = configparser.ConfigParser()
+        config.read(filetoread)
+
+        for each_section in config.sections():
+
+            if each_section not in newdict.keys():
+                newdict[each_section] = dict()
+
+                for (each_key, each_val) in config.items(each_section):
+                    if each_key not in newdict[each_section].keys():
+                        newdict[each_section][each_key] = each_val
+        return newdict
 
 
 pythonserver = PythonSERVer()
-
-
-def config_file_to_dict(filetoread):
-
-    newdict = dict()
-
-    # Read configuration
-    config = configparser.ConfigParser()
-    config.read(filetoread)
-
-    for each_section in config.sections():
-
-        if each_section not in newdict.keys():
-            newdict[each_section] = dict()
-
-            for (each_key, each_val) in config.items(each_section):
-                if each_key not in newdict[each_section].keys():
-                    newdict[each_section][each_key] = each_val
-    return newdict
