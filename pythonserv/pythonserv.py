96cba8020b6b4e483b455e5dc8f7d344549a39ad
deathbybandaid <sam@deathbybandaid.net>
2019-10-10 15:02:58 -0400

    test for line format

--- pythonserv/pythonserv.py
+++ pythonserv/pythonserv.py
@@ -47,6 +47,33 @@ class PythonSERVer():
         return line
 
     def writeline(self, line, *args, **kwargs):
+        if isinstance(line, str):
+            if '{}' in line:
+                line = line.format(*args, **kwargs)
+            else:
+                params = list()
+                trailing = False
+                semi_trailing = False
+                for param in args:
+                    if trailing:
+                        raise ValueError(
+                            'writeline: Parameter with space character should be used once and at the last position')
+                    param = str(param)
+                    if param.startswith('+') or param.startswith('-'):
+                        if semi_trailing:
+                            raise ValueError(
+                                'writeline: Parameter starts with + or - character should be used once')
+                        semi_trailing = True
+                        params.append(param)
+                    elif (param == '') or (' ' in param):
+                        params.append(':' + param)
+                        trailing = True
+                    else:
+                        params.append(param)
+                if len(params) > 0:
+                    line = '{} {}'.format(line, ' '.join(params))
+        if '\n' in line:
+            raise ValueError('writeline: Message should not be multi-lined')
         print("writing     " + str(line))
         self.writer.write(line.encode("utf-8", errors='surrogateescape') + b'\r\n')
 
